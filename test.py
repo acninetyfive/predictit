4bc70c0a32845114971a79429fbc1a74924d977a
Gabe Stanton <acninetyfive@gmail.com>
2019-07-03 10:46:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import numpy as np
-from pulp import *
+import pulp
+import urllib.request, json 
+
+'''
+with urllib.request.urlopen("https://www.predictit.org/api/marketdata/all/") as url:
+    data = json.loads(url.read().decode())
+    print(data)
+'''
 
 class Bet:
 	def __init__(self, name, cost):
@@ -11,45 +18,55 @@ class Bet:
 def create_problem(bets):
 	lp_vars = {}
 
-	problem = pulp.LpProblem("Problem", LpMaximize)
+	problem = pulp.LpProblem("Problem", pulp.LpMaximize)
 
+	profit = pulp.LpVariable('profit', lowBound=0, cat='Continuous')
+	
+	problem += profit
+	
 	for b in bets:
-		lp_vars[b.name] = pulp.LpVariable(b.name, lowBound=0, cat='Integer')
-	lp_vars["profit"] = pulp.LpVariable('profit', lowBound=0, cat='Continuous')
+		lp_vars[bets[b].name] = pulp.LpVariable(bets[b].name, lowBound=0, cat='Integer')
+	
+	#print(lp_vars)
+	for v in lp_vars:
+		if v is not "profit":
+			expr = pulp.LpAffineExpression([(lp_vars[x], bets[x].profit) if x != v else (lp_vars[x], -1 * bets[x].cost) for x in lp_vars] + [(profit, -1)])
+			c = pulp.LpConstraint(expr, 1) #1 indicates the expression should be >= 0
+			problem += c
 
+	max_profit = pulp.LpAffineExpression([(lp_vars[x], bets[x].profit) for x in lp_vars])
+	max_profit += -850
+	c = pulp.LpConstraint(max_profit, -1) 
+	problem += c
 
+	return problem
 
 x_bet = Bet("x", .98)
 y_bet = Bet("y", .01)
 
 
+nom_bets = {"kamala": Bet("kamala", .75), "biden": Bet("biden", .80), "warren": Bet("warren", .82), 
+		"sanders": Bet("sanders", .87), "buttigieg": Bet("buttigieg", .88), "yang": Bet("yang", .93),
+		"booker": Bet("booker", .98), "gabbard": Bet("gabbard", .98), "castro": Bet("castro", .97),
+		"klobuchar": Bet("klobuchar", .98), "orourke": Bet("orourke", .98), "clinton": Bet("clinton", .98),
+		"a": Bet("a", .99), "b": Bet("b", .99), "c": Bet("c", .99), "d": Bet("d", .99), "e": Bet("e", .99)}
+
+pres_bets = {"trump": Bet("trump", .56), "kamala": Bet("kamala", .84), "biden": Bet("biden", .87), "warren": Bet("warren", .88), 
+		"sanders": Bet("sanders", .92), "buttigieg": Bet("buttigieg", .92), "yang": Bet("yang", .94),
+		"booker": Bet("booker", .96), "gabbard": Bet("gabbard", .96), "a": Bet("a", .99), "b": Bet("b", .99), "c": Bet("c", .99)}
 
 
+problem = create_problem(pres_bets)
 
+#print(problem)
 
-test_problem = pulp.LpProblem("Test", LpMaximize)
+print(problem.solve())
 
-
-x = pulp.LpVariable('x', lowBound=0, cat='Integer')
-y = pulp.LpVariable('y', lowBound=0, cat='Integer')
-z = pulp.LpVariable('z', lowBound=0, cat='Continuous')
-
-#objective
-test_problem += z, "Z"
-
-
-# Constraints
-test_problem += x_bet.profit * x - y_bet.cost * y >= z
-test_problem += -x_bet.cost * x + y_bet.profit * y >= z 
-test_problem += x_bet.profit * x + y_bet.profit * y <= 850
-
-test_problem.solve()
-
-for variable in test_problem.variables():
+for variable in problem.variables():
 	print(variable.name, variable.varValue)
 
 
 
 
-
-
+#API Call for all markets
+#https://www.predictit.org/api/marketdata/all/
