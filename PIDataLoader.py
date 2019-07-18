ca259d82a45f4b38607c9f19c301c861ea2228cf
Gabe Stanton <acninetyfive@gmail.com>
2019-07-18 10:37:22 -0400

    Update PIDataLoader.py

--- PIDataLoader.py
+++ PIDataLoader.py
@@ -12,35 +12,14 @@ import time
 
 db = firestore.Client.from_service_account_json(
         'PredictIt-9ad78fc7db12.json')
-'''
-# Project ID is determined by the GCLOUD_PROJECT environment variable
-#db = firestore.Client()
 
-doc_ref = db.collection('users').document('alovelace')
-doc_ref.set({
-    'first': 'Ada',
-    'last': 'Lovelace',
-    'born': 1815
-})
-
-doc_ref = db.collection('users').document('aturing')
-doc_ref.set({
-    'first': 'Alan',
-    'middle': 'Mathison',
-    'last': 'Turing',
-    'born': 1912
-})
-
-users_ref = db.collection(u'users')
-docs = users_ref.get()
-
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
-
-
-'''
 execs = 0
+fails = 0
 def load_data():
+
+    global execs 
+    global fails
+
     url = "https://www.predictit.org/api/marketdata/all/"
     try:
         response = requests.get(url)
@@ -54,23 +33,25 @@ def load_data():
     else:
         print('Success!')
 
-    data = json.loads(response.content)
-    #print(len(data["markets"]))
-    c = 0
-    for x in data["markets"]:
-        try:
-            doc_ref = db.collection(x["shortName"].replace('/', '.')).document(x["timeStamp"])
-            doc_ref.set(x)
-            c += 1
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-            print(x["shortName"])
+        data = json.loads(response.content)
+        added_markets = []
 
-    #print (c)
-    global execs 
-    execs += 1
-    print("ran " + str(execs) + " times")
-    return 1
+        for x in data["markets"]:
+            if "tweet" in x["shortName"]:
+                try:
+                    doc_ref = db.collection(x["shortName"].replace('/', '.')).document(x["timeStamp"])
+                    doc_ref.set(x)
+                    added_markets.append(x["shortName"])
+                except Exception as err:
+                    print(f'Other error occurred: {err}')
+                    print(x["shortName"])
+
+        execs += 1
+        print("ran " + str(execs) + " times, failed " + str(fails) + " times")
+        return 1
+
+    fails += 1
+    print("ran " + str(execs) + " times, failed " + str(fails) + " times")
 
 schedule.every(60).seconds.do(load_data)
 
